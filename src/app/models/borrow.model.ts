4917a3f8c48baf5dd5fa2fb5dbf2d92d9089d676
Hera <mostafizhera@gmail.com>
2025-06-21 01:52:10 +0600

    created borrow model and borrow interface

---
+++ src/app/models/borrow.model.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose"
+import { IBorrow } from "../interfaces/borrow.interface"
+
+const borrowSchema = new Schema<IBorrow>({
+        book: {
+        type: Schema.Types.ObjectId,
+        ref: "Book",
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    dueDate: {
+        type: Date,
+        required: true,
+        trim: true
+    }
+}, { versionKey: false, timestamps: true }
+)
+
+export const Borrow = model<IBorrow>("Borrow", borrowSchema)
